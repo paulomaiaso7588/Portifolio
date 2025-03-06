9133cebc7ad09c5672289f439ae312a64cc81b76
PhCdn <114597146+paulomaiaso7588@users.noreply.github.com>
2025-03-06 00:31:47 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const contatoSection = document.querySelector('#contato');
+    
+    contatoSection.addEventListener('click', function() {
+        alert('Você clicou na seção de Contato!');
+    });
+});
